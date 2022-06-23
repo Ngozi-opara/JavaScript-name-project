fa39ebe3776f924bdacef5aaa56d5bfa9169842f
Hannah--- <87046881+Ngozi-opara@users.noreply.github.com>
2022-06-23 12:21:02 +0100

    Add files via upload
    Showing name with JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,10 @@
+let myName = "Hannah";
+let myHeight = "170cm";
+let myCountry = "Nigeria";
+alert("Hello. My name is " + myName + ". I am " + myHeight + " in height. I am from " + myCountry + ".");
+
+let myOtherName = "Hello. My name is Hannah.";
+let myOtherHeight = "I am 170cm in height.";
+let myOtherCountry = "I am from Nigeria.";
+let result = myOtherName.concat(" ", myOtherHeight, " ",  myOtherCountry);
+alert(result);
